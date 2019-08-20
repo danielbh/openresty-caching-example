2c950642273945e20e64c1195d2401acaa98f6e2
Daniel Hollcraft <danielhollcraft@gmail.com>
2019-08-19 20:35:28 -0400

    adding to example

--- go/main.go
+++ go/main.go
@@ -6,9 +6,23 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
+
+	returnFailure := false
+
+	// Request fails every other time to rest fallback cache
+	http.HandleFunc("/failodd", func(w http.ResponseWriter, r *http.Request) {
+		if returnFailure {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Uh oh something bad happened")
+		} else {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "Everything is fine")
+		}
+
+		returnFailure = !returnFailure
 	})
+
+	//TODO: javacript with fingerprint example
 
 	http.ListenAndServe(":8000", nil)
 }
